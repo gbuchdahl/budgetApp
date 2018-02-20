60238856ab2c7145e11849c25dc58e70cd2e9455
gbuchdahl <gbuchdahl@gmail.com>
2018-02-19 23:11:29 -0700

    added first event listeners

--- app.js
+++ app.js
@@ -22,7 +22,23 @@ var UIController = (function () {
 //GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+  var ctrlAddItem = function(){
+    // 1. Get input data
+    // 2. Add the item to the budget controller
+    // 3. Add a new item to the UI
+    // 4. Calculate the budget
+    // 5. display the budget
+    console.log("ENTER was pressed.")
+  }
 
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
+  document.addEventListener('keypress', function(event) {
+
+      if (event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+      }
+
+  });
 
 })(budgetController, UIController);
