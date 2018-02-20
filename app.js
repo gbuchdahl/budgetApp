8ebc3299973e2a360fdebd38ddb6cea982ac395e
gbuchdahl <gbuchdahl@gmail.com>
2018-02-19 22:58:30 -0700

    added some comments

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+
+//BUDGET CONTROLLER
 var budgetController = (function () {
 
   //Some code
 
 })();
 
+
+
+
+//UI CONTROLLER
 var UIController = (function () {
 
   //some code
 
 })();
 
+
+
+
+//GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-  
+
 
 
 })(budgetController, UIController);
