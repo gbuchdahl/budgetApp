1307166c13d0a028b7cd707d0f5e36270e24fbd2
gbuchdahl <gbuchdahl@gmail.com>
2018-02-20 17:08:47 -0700

    initialized page to 0

--- app.js
+++ app.js
@@ -254,6 +254,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     init: function() {
       console.log("Application has started.");
       setUpEventListeners();
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
     }
   };
 
