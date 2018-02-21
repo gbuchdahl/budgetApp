01d379eea10521cfe2ffc3d49efccaf8ad5848fe
gbuchdahl <gbuchdahl@gmail.com>
2018-02-20 20:55:16 -0700

    added calc/getPercentage() to Expense prototype

--- app.js
+++ app.js
@@ -8,7 +8,21 @@ var budgetController = (function () {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
+
+  Expense.prototype.calcPercentage = function (totalIncome) {
+
+    if (totalIncome > 0){
+      this.percentage = Math.round((this.value / totalIncome) * 100);
+    } else{
+      this.percentage = -1;
+    }
+  };
+
+  Expense.prototype.getPercentage = function() {
+    return this.percentage;
+  }
 
   var Income = function(id, description, value){
     this.id = id;
@@ -102,7 +116,7 @@ var budgetController = (function () {
 
     calculatePercentages: function () {
 
-      
+
 
     }
 
